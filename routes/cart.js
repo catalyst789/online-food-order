b6f1f8d061fac37a553a29d0e0bdbb0b8c9d9c36
Jalaj Kumar Gaur <61074719+catalyst789@users.noreply.github.com>
2023-06-09 00:20:21 +0530

    Update database config

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/cart');
+// mongoose.connect('mongodb://localhost/cart');
 // mongoose.connect('mongodb://admin:admin007@ds157964.mlab.com:57964/cafeteria')
+
+mongoose.connect('mongodb+srv://jkthecoder:jkTheCoder@cluster0.va5hj.mongodb.net/skilllssharedatabase?retryWrites=true&w=majority',  {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then( () => console.log('Database Connected ..!'))
+.catch( err => console.log(err));
 
 var cartSchema = mongoose.Schema({
   prdc: Number
